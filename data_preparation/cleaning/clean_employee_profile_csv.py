ad4467f15e3e69ec9ef97cdf0f63d64acac9a009
hd-rk <hdn314159@gmail.com>
2023-03-18 01:29:12 -0700

    feat: clean employee csv

--- data_preparation/cleaning/clean_employee_profile_csv.py
+++ data_preparation/cleaning/clean_employee_profile_csv.py
@@ -22,26 +22,28 @@ invalid_file_count = 0
 
 employee_count = 0
 
-def preprocess_file(folder_name, filename):
+def clean_csv(input_file, output_dir):
     rows = []
     columns = []
-    print("Opening file: "+filename)
+    print(f"Opening file: {input_file}")
     # Open the CSV file
-    with open(os.path.join(folder_name, filename), 'r', encoding='utf-8') as csvfile:
+    with open(input_file, 'r', encoding='utf-8') as csvfile:
         # Read the entire contents of the CSV file as a string
         file_content = csvfile.read()
+        
         # Check if the file contains any HTML tags
         if re.search(html_tag_pattern, file_content):
             # Skip to the next file if the file contains HTML content
-            print(f"HTML element found in file {filename} Skipping")
-            return 0
+            print(f"skipping: HTML element found in file {input_file}")
+            return None
+        
         csvfile.seek(0)  # move file pointer to start of file
         # Create a CSV reader object
         reader = csv.reader(csvfile, delimiter=',')
         try:
-            foundColumn = False
+            foundColumnLabelRow = False
             # Get the first row of the CSV file
-            while not foundColumn:
+            while not foundColumnLabelRow:
                 first_row = next(reader)
                 if len_character_threshold <= len(first_row[0]):
                     print("skipping: too many characters in first row cell")
@@ -55,26 +57,31 @@ def preprocess_file(folder_name, filename):
                     print("found label row")
                     print(first_row)
                     columns = first_row
-                    foundColumn = True
+                    foundColumnLabelRow = True
+            print(f"label row found: {columns}")
             while True:
                 row = next(reader)
                 if len(row) <= len(columns):
                     if "".join(row).strip() == "":
-                        print("skip empty row")
+                        print("skipping empty row")
                         continue
                     rows.append(row)
                 else:
                     rows.append(row[:len(columns)])
         except StopIteration as e:
-            print(f"Reached end of file: {filename}")
+            print(f"Reached end of file: {input_file}")
             # Skip this file if it has less than one row
-            if len(columns) > 0:
+            if len(columns) > 0 and len(rows) > 0:
                 df = pd.DataFrame(rows, columns=columns)
-                df.to_csv('output/'+filename)
-            return len(rows)
+                output_file = input_file.split('/')[-1].split('.')[0]
+                output_path = f"{output_dir}/{output_file}-cleaned.csv"
+                print(f"writing output file to {output_path}")
+                df.to_csv(output_path)
+                return output_path
+            return None
         except Exception as e:
-            print(f"Exception error file not valid: {e} in {filename}")
-            return 0
+            print(f"Exception error file not valid: {e} in {input_file}")
+            return None
     # Write the list of first rows to a text file
     # with open('output.txt', 'w') as txtfile:
     #     for row in first_rows:
@@ -83,12 +90,12 @@ def preprocess_file(folder_name, filename):
 
     # print("invalid_first_row_count: " + str(invalid_first_row_count))
     # print("invalid_file_count: " + str(invalid_file_count))
-    print(first_rows)
+    # print(first_rows)
 
-for filename in os.listdir(folder_path):
-    print(filename)
-    count = preprocess_file('csv/employee_csv_20230315', filename)
-    employee_count += count
-    print(count)
+# for filename in os.listdir(folder_path):
+#     print(filename)
+#     count = preprocess_file('csv/employee_csv_20230315', filename)
+#     employee_count += count
+#     print(count)
 
-print(f"Toal Count: {employee_count}")
+# print(f"Toal Count: {employee_count}")
