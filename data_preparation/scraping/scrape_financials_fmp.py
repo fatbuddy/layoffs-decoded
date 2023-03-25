59f649bbc9d9973b640e5be5eb3a3b5783c0e028
hd-rk <hdn314159@gmail.com>
2023-03-25 14:47:54 -0700

    fix: handle empty fin data error

--- data_preparation/scraping/scrape_financials_fmp.py
+++ data_preparation/scraping/scrape_financials_fmp.py
@@ -11,15 +11,19 @@ FMP_API_ENDPOINT="https://financialmodelingprep.com/api"
 FMP_API_KEY=""
 
 def process_fmp_financial_statements(file_path):
-    raw_df = pd.read_csv(file_path)
-    raw_df.rename({"Unnamed: 1": "metric"}, axis=1, inplace=True)
-    df = raw_df[~raw_df['metric'].isna()]
-    df = df.drop("date", axis=1)
-    df = df.rename({"metric": "date"}, axis=1)
-    df = df.set_index("date").transpose()
-    df.index = df.index.map(lambda s: re.sub(r"_Q[1-4]", "", s))
-    df = df.sort_index(ascending=False)
-    return df
+    try:
+        raw_df = pd.read_csv(file_path)
+        raw_df.rename({"Unnamed: 1": "metric"}, axis=1, inplace=True)
+        df = raw_df[~raw_df['metric'].isna()]
+        df = df.drop("date", axis=1)
+        df = df.rename({"metric": "date"}, axis=1)
+        df = df.set_index("date").transpose()
+        df.index = df.index.map(lambda s: re.sub(r"_Q[1-4]", "", s))
+        df = df.sort_index(ascending=False)
+        return df
+    except pd.errors.EmptyDataError as err:
+        print(err)
+        return None
 
 
 def pull_fmp_financial_statements(stock_symbols, output_dir, api_key):
@@ -28,6 +32,7 @@ def pull_fmp_financial_statements(stock_symbols, output_dir, api_key):
     output_files = []
     for raw_sym in stock_symbols:
         sym = raw_sym.replace("/", "-")
+        sym = re.sub(r"\^[A-Z]$", "", sym)
         merged_file_name = f"{output_dir}/{sym}-all.csv"
         merged_df = None
         for stmt_type in statment_types:
@@ -51,10 +56,11 @@ def pull_fmp_financial_statements(stock_symbols, output_dir, api_key):
                     if len(row) >= 3 and len(row[2]) > 0:
                         writer.writerow(row)
             df = process_fmp_financial_statements(raw_file_path)
-            if merged_df is not None:
-                merged_df = merged_df.join(other=df, how="outer", rsuffix=f"_{stmt_type}")
-            else:
-                merged_df = df
+            if df is not None:
+                if merged_df is not None:
+                    merged_df = merged_df.join(other=df, how="outer", rsuffix=f"_{stmt_type}")
+                else:
+                    merged_df = df
             # df.to_csv(processed_file_path)
             # output_files.append(processed_file_path)
             sleep(randint(1,3))
