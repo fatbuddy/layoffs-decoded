f277b7e610d2131d43a14cb46f6dc53af1145162
tylerdsilva <tylerdsilva1@gmail.com>
2023-04-07 15:10:26 -0700

    Question 1 FE and q2_rnn improvement

--- layoffs-decoded/src/components/Question1.jsx
+++ layoffs-decoded/src/components/Question1.jsx
@@ -10,7 +10,9 @@ const valueFormatter = (number) =>
 export default function Question1() {
   const [precovidStatus, setPrecovidStatus] = useState('');
   const [precovid, setPrecovid] = useState([]);
+  const [covidStatus, setCovidStatus] = useState('');
   const [covid, setCovid] = useState([]);
+  const [postcovidStatus, setPostcovidStatus] = useState('');
   const [postcovid, setPostcovid] = useState([]);
 
   function callApis(technique) {
@@ -21,28 +23,42 @@ export default function Question1() {
     .then(()=>setPrecovidStatus('Success'))
     .catch(()=>setPrecovidStatus('Error'));
 
+    setCovidStatus('Loading');
     fetch(process.env.REACT_APP_API_PROXY + '/q1_covid_'+ technique + '?limit=10')
     .then(result => result.json())
-    .then(data => setCovid(data.payload));
+    .then(data => setCovid(data.payload))
+    .then(()=>setCovidStatus('Success'))
+    .catch(()=>setCovidStatus('Error'));
 
+    setPostcovidStatus('Loading');
     fetch(process.env.REACT_APP_API_PROXY + '/q1_postcovid_'+ technique + '?limit=10')
     .then(result => result.json())
-    .then(data => setPostcovid(data.payload));
+    .then(data => setPostcovid(data.payload))
+    .then(()=>setPostcovidStatus('Success'))
+    .catch(()=>setPostcovidStatus('Error'));
   }
 
   useEffect(()=>{
     setPrecovidStatus('Loading');
     fetch(process.env.REACT_APP_API_PROXY + '/q1_precovid_pearson?limit=10')
     .then(result => result.json())
-    .then(data => setPrecovid(data.payload));
+    .then(data => setPrecovid(data.payload))
+    .then(()=>setPrecovidStatus('Success'))
+    .catch(()=>setPrecovidStatus('Error'));
 
+    setCovidStatus('Loading');
     fetch(process.env.REACT_APP_API_PROXY + '/q1_covid_pearson?limit=10')
     .then(result => result.json())
-    .then(data => setCovid(data.payload));
+    .then(data => setCovid(data.payload))
+    .then(()=>setCovidStatus('Success'))
+    .catch(()=>setCovidStatus('Error'));
 
+    setPostcovidStatus('Loading');
     fetch(process.env.REACT_APP_API_PROXY + '/q1_postcovid_pearson?limit=10')
     .then(result => result.json())
-    .then(data => setPostcovid(data.payload));
+    .then(data => setPostcovid(data.payload))
+    .then(()=>setPostcovidStatus('Success'))
+    .catch(()=>setPostcovidStatus('Error'));
     }, []);
 
   return (
@@ -62,7 +78,11 @@ export default function Question1() {
         >
         <Card>
             <Title>Precovid</Title>
-            {precovidStatus === 'Loading' && <Subtitle>Loading...</Subtitle>}
+            {precovidStatus === 'Loading' && <DonutChart
+            className="mt-6"
+            showLabel={true}
+            label="Loading..."
+            />}
             {precovidStatus === 'Success' && 
             <DonutChart
             className="mt-6"
@@ -76,6 +96,12 @@ export default function Question1() {
         </Card>
         <Card>
             <Title>Covid</Title>
+            {covidStatus === 'Loading' && <DonutChart
+            className="mt-6"
+            showLabel={true}
+            label="Loading..."
+            />}
+            {covidStatus === 'Success' &&
             <DonutChart
             className="mt-6"
             data={covid}
@@ -83,10 +109,16 @@ export default function Question1() {
             index="name"
             valueFormatter={valueFormatter}
             colors={["slate", "violet", "indigo", "rose", "cyan", "amber", "teal", "orange", "fuchsia", "pink"]}
-            />
+            />}
         </Card>
         <Card>
             <Title>Postcovid</Title>
+            {postcovidStatus === 'Loading' && <DonutChart
+            className="mt-6"
+            showLabel={true}
+            label="Loading..."
+            />}
+            {postcovidStatus === 'Success' &&
             <DonutChart
             className="mt-6"
             variant="pie"
@@ -95,7 +127,7 @@ export default function Question1() {
             index="name"
             valueFormatter={valueFormatter}
             colors={["slate", "violet", "indigo", "rose", "cyan", "amber", "teal", "orange", "fuchsia", "pink"]}
-            />
+            />}
         </Card>
         </Grid>
         <Grid
